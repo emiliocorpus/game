58b2159dceb3e92f4bc322f75dedb6d535baaa62
emiliocorpus <emiliocorpus@gmail.com>
2016-09-30 11:59:52 -0700

    sets up initial game environment

--- app/assets/javascripts/components/game.es6.jsx
+++ app/assets/javascripts/components/game.es6.jsx
@@ -1,11 +1,39 @@
 class Game extends React.Component {
 
   componentDidMount() {
-  let canvas = ReactDOM.findDOMNode(this.refs.myCanvas);
-  let ctx = canvas.getContext('2d');
+	let canvas = ReactDOM.findDOMNode(this.refs.myCanvas);
+	let ctx = canvas.getContext('2d');
 
-  canvas.height = 700
-  canvas.width = 600
+
+	var CANVAS_WIDTH = 600;
+	var CANVAS_HEIGHT = 700;
+	var FPS = 30;
+
+
+	canvas.height = CANVAS_WIDTH
+	canvas.width = CANVAS_HEIGHT
+
+	var textX = 50;
+	var textY = 50;
+	
+	setInterval(function() {
+	  update();
+	  draw();
+	}, 1000/FPS);
+
+
+	draw()
+
+	function update() {
+		textX += 1;
+		textY += 1;
+	}
+	function draw() { 
+		ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+		ctx.fillStyle = "#000"; // Set color to black
+		ctx.fillText("Sup Bro!", textX, textY);
+	}
+
 
   }
 
