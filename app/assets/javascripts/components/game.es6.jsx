726c573b0924f9f5e2ea28fc1cd0a1c3c103fbe6
emiliocorpus <emiliocorpus@gmail.com>
2016-09-30 11:35:23 -0700

    adds initial canvas setup

--- app/assets/javascripts/components/game.es6.jsx
+++ app/assets/javascripts/components/game.es6.jsx
@@ -1,6 +1,20 @@
 class Game extends React.Component {
+
+  componentDidMount() {
+  let canvas = ReactDOM.findDOMNode(this.refs.myCanvas);
+  let ctx = canvas.getContext('2d');
+
+  canvas.height = 700
+  canvas.width = 600
+
+  }
+
   render () {
-    return <div />;
+    return (
+    	<div className="game-container">
+            <canvas ref="myCanvas" />
+        </div>	
+    )
   }
 }
 
